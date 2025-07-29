12669469a8d815c346c876b5cedc45d389c2ba71
ECO-HZ <zhubiaoooo@outlook.com>
2025-07-29 23:35:21 +0800

    Update Figure S5.R

--- Code folder/Figure S5.R
+++ Code folder/Figure S5.R
@@ -1,86 +1,104 @@
 ################################################################################
-################################## Figure S5###################################
+################################## Figure S5 ###################################
 ################################################################################
 
 # Loading the R packages
+library(ggplot2)
 library(openxlsx)
 library(dplyr)
-library(tidygeocoder)
-library(sf)
-library(ggplot2)
-library(ggspatial)
-library(gt)
-library(tidyverse)
-library(glue)
-library(ggtree)
+library(patchwork)
+library(vegan)
+library(car)
 library(phytools)
 
-# Loading seed collection location information
-seed_data <- read.xlsx("Site_information_seed.xlsx", sheet = "site_map", rowNames = F, colNames = T)
+# Custom Style
+mytheme <- theme(panel.background = element_rect(fill = 'transparent', colour='black'),
+                 legend.position = 'none',
+                 panel.grid=element_blank(), 
+                 legend.title = element_blank(),
+                 legend.text = element_text(size = 8),
+                 legend.background = element_rect(fill = NA), #axis.ticks.length = unit(0.4,"lines"), 
+                 axis.ticks = element_line(color='black'),
+                 axis.line = element_line(colour = "black"), 
+                 axis.title.x = element_text(colour='black', size=13),
+                 axis.title.y = element_text(colour='black', size=13),
+                 axis.text = element_text(colour='black',size=11),
+                 plot.tag = element_text(size = 14, face = "bold")) 
 
-# Obtain latitude and longitude information of the collection location
-options(amap_key = '989a66c96e4750756c11390fa782d8bc')
-seed_site_data <- seed_data %>% 
-  geocode("Address", method = 'arcgis', lat = latitude, 
-          long = longitude, full_results = TRUE) %>% as.data.frame()
-#head(seed_site_data)
-colnames(seed_site_data)[c(4,5)] <- c("Latitude", "Longitude")
-seed_site_data <- seed_site_data %>% left_join(seed_data) 
+family_color <- c("Poaceae"="#332500","Cyperaceae"="#542D20","Solanaceae" ="#994240",
+                  "Verbenaceae" = "#D75C4D", "Acanthaceae" = "#E68C51", "Lamiaceae" ="#F59D52", 
+                  "Asteraceae" = "#EFBA55", "Polygonaceae" ="#FCD170", "Phytolaccaceae"="#FEE1B0", 
+                  "Caryophyllaceae" = "#C5E0D0", "Amaranthaceae" = "#ABDCE0", "Euphorbiaceae" ="#7FC5D8",
+                  "Onagraceae"="#73BCD5", "Urticaceae" = "#528FAC", "Fabaceae" = "#376694", "Malvaceae" = "#1F466F")
 
-# Loading China map
-china_map <- sf::st_read("https://geo.datav.aliyun.com/areas_v3/bound/100000_full.json") 
+family_order <- c("Poaceae", "Cyperaceae", "Solanaceae", "Verbenaceae", "Acanthaceae", "Lamiaceae", 
+                  "Asteraceae", "Polygonaceae", "Phytolaccaceae", "Caryophyllaceae", "Amaranthaceae", 
+                  "Euphorbiaceae", "Onagraceae", "Urticaceae", "Fabaceae", "Malvaceae")
 
-################################# Figure S4B ###################################
-ggplot(china_map)+
-  geom_sf(data = china_map, fill = "grey95",size = 1) + 
-  xlim(105, 122)+ ylim(18, 42)+ 
-  ggnewscale::new_scale_fill() + 
-  annotate("text", x = 106, y = 32, label = "China", size = 4, fontface = "bold") + 
-  geom_point(data = seed_site_data, aes(x = Longitude, y = Latitude),
-             size = 3, alpha = 1, shape = 21, color = "black", fill = "#32B0A5") + 
-  annotation_scale(location = "br", style = "ticks", line_width = 1.2, pad_y = unit(0.5, "cm"), text_cex = 0.9) + 
-  annotation_north_arrow(location = "tl", which_north = "true", height = unit(1, "cm"), width = unit(1, "cm"),
-                         pad_x = unit(0.3, "cm"), pad_y = unit(0.3, "cm"), style = north_arrow_fancy_orienteering) +
-  theme_bw() + 
-  theme(panel.background = element_blank(),
-        panel.grid = element_blank(),
-        axis.line.x = element_line(size = 0.5, colour = "NA"),
-        axis.line.y = element_line(size = 0.5, colour = "NA"),
-        axis.ticks = element_line(color = "black"),
-        axis.text = element_text(color = "black", size = 11),
-        legend.position = "right",
-        legend.background = element_blank(),
-        legend.key = element_blank(),
-        legend.text = element_text(size=9),
-        legend.title = element_text(size=10),
-        plot.tag = element_text(size = 14, face = "bold"))+
-  labs(x = " ", y = " ", tag = "(a)") +
-  ggrepel::geom_text_repel(mapping = aes(x = Longitude,y = Latitude,label = City), data = seed_site_data, size = 2.8,
-                           segment.color = "black", color = "black",direction = "both",box.padding = 0.6,
-                           max.overlaps = getOption("ggrepel.max.overlaps", default = 25))
-                           
+# Loading the greenhouse experiment data
+Green_group <- read.xlsx("Greenhouse_data_group.xlsx", sheet = "green_group", colNames = T, rowNames = T)
+Green_group$Sample_ID <- rownames(Green_group)
 
-################################# Figure S4B ###################################
-#tree <- read.newick("iq_tree.NEWICK")
+Green_otu_raw <- read.xlsx("Greenhouse_data_raw_ASVs.xlsx", sheet = "raw_ASVs", colNames = T, rowNames = T)
+Green_otu_raw <- Green_otu_raw[ ,Green_group$Sample_ID]
+dim(Green_otu_raw)
+
+# Richness of overall fungi
+Green_richness <- as.data.frame(specnumber(t(Green_otu_raw)))
+Green_richness$Sample_ID <- rownames(Green_richness); colnames(Green_richness)[1] <- "Green_SR"
+
+# Effect of family and species on fungal richness
+Green_exp_data <- Green_group %>% left_join(Green_richness)
+mod <- lm(Green_SR ~ Family/Species , data = Green_exp_data)
+Table_Fig_S3 <- as.data.frame(car::Anova(mod, type = 2))
+Table_Fig_S3 <- Table_Fig_S3[-which(rownames(Table_Fig_S3) == "Residuals"),]
+Table_Fig_S3$p.adj <- p.adjust(Table_Fig_S3$`Pr(>F)`, method = "holm")
+Table_Fig_S3$p.adj <- sprintf("%.3f", Table_Fig_S3$p.adj)
+Table_Fig_S3$`Pr(>F)` <- round(Table_Fig_S3$`Pr(>F)`, 3)
+Table_Fig_S3$`F` <- round(Table_Fig_S3$`F value`, 2)
+rownames(Table_Fig_S3) <- c("Family", "Species")
+Table_Fig_S3$df <- c("15,108", "38,108")
+print(Table_Fig_S3[ ,c("df","F","Pr(>F)","p.adj")])
+
+# loading phylogenetic tree
 tree <- read.newick("IQ_tree_plant_2025.NEWICK")
 to_drop <- c("Amborella_trichopoda","")
 tree <- drop.tip(as.phylo(tree), to_drop) 
-plot(tree)
-ggtree(tree, size = 0.4, color = "black", branch.length = "branch.length", ladderize = F) + 
-  geom_tiplab(aes(label = sub("_", " ", label)), size = 2, offset = 0.01,
-              fontface = "italic", linetype = "dotted", align = TRUE) + 
-  xlim(0,8)-> p.tree; p.tree
 
-ggtree::rotate(p.tree, node = 56)
+################################# Figure S2A ###################################
+Green_exp_data$Family <- factor(Green_exp_data$Family, levels = rev(family_order))
 
-# Add collection point information
-seed_site_infor = read.xlsx("Site_information_seed.xlsx", sheet = "sp_site", rowNames = F, colNames = T)
-rownames(seed_site_infor) = seed_site_infor$Species
-seed_site_infor = seed_site_infor[rev(tree$tip.label), ]
-seed_site_infor$Species = NULL
-gt(seed_site_infor)
+ggplot(Green_exp_data,aes(x = Family, y = Green_SR, fill = Family)) + 
+  geom_boxplot(width = 0.6,alpha = 0.75) + 
+  #scale_color_manual(values = family_color) +
+  scale_fill_manual(values = family_color) +
+  annotate("text", x = 16, y = 120,
+           label = expression("F"[15*","*108] == paste("1.36")), size = 4) + 
+  annotate("text", x = 15.5, y = 120,
+           label = expression(italic(p) == 0.216), size = 4) +
+  mytheme + 
+  coord_flip() + 
+  labs(x = NULL, y = "Overall fungal richness", tag = "(a)") -> p1; p1
 
-### Notice that,
-### For more picture details, we have further adjusted it in Adobe illustrator.
+################################# Figure S2B ###################################
+Green_exp_data$Species <- gsub("_", " ", Green_exp_data$Species)
+Green_exp_data$Species <- factor(Green_exp_data$Species, levels = gsub("_", " ", tree$tip.label))
+#str(Green_exp_data)
+#Green_exp_data$sig <- ifelse(Green_exp_data$Origin == "Native"," ","*")
 
+ggplot(Green_exp_data,aes(x = Species, y = Green_SR, fill = Family)) + 
+  geom_boxplot(width = 0.6, alpha = 0.75) + 
+  #scale_color_manual(values = family_color) +
+  scale_fill_manual(values = family_color) +
+  annotate("text", x = 53, y = 120,
+           label = expression("F"[38*","*108] == paste("1.37")), size = 4) + 
+  annotate("text", x = 51, y = 120,
+           label = expression(italic(p) == 0.216), size = 4) +
+  mytheme + 
+  theme(axis.text.y = element_text(face = "italic",size=10)) + 
+  coord_flip() + 
+  labs(x = NULL, y = "Overall fungal richness", tag = "(b)") -> p2; p2
+
+# Combination 
+p1|p2 -> Fig_S5; Fig_S5
 
