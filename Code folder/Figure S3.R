2959eafa133a2122b31c5cc0f387a4aec4f397ad
ECO-HZ <zhubiaoooo@outlook.com>
2025-12-07 19:53:05 +0800

    Update Figure S3.R

--- Code folder/Figure S3.R
+++ Code folder/Figure S3.R
@@ -1,189 +1,215 @@
 ################################################################################
-################################## Figure S3 ###################################
+################################# Figure S3 ####################################
 ################################################################################
-# Robustness evaluation of fungal composition distinctiveness index (based on greenhouse exp.)
-
 # Loading the R packages
-library(betapart)
 library(openxlsx)
-library(dplyr)
-library(Rmisc)
-library(reshape2)
-library(emmeans)
-library(multcomp)
-library(ggplot2)
+library(ggvenn)
+library(ggridges)
 
-# Custom style
-mytheme <- theme(panel.background = element_rect(fill = 'transparent', colour = 'black'),
-                 legend.position = 'none',
-                 panel.grid = element_blank(), 
-                 legend.title = element_blank(),
-                 legend.text = element_text(size = 8),
-                 legend.background = element_rect(fill = NA), #axis.ticks.length = unit(0.4,"lines"), 
-                 axis.ticks = element_line(color = 'black'),
-                 axis.line = element_line(colour = 'black'), 
-                 axis.title.x = element_text(colour = 'black', size = 13),
-                 axis.title.y = element_text(colour = 'black', size = 13),
-                 axis.text = element_text(colour = 'black', size = 11),
-                 plot.tag = element_text(size = 14, face = "bold")) 
+# Soil sample grouping information in field
+Field_group <- read.xlsx("Field_data_group.xlsx", sheet = "Field_group", rowNames = T, colNames = T)
+Field_group$Sample_ID <- rownames(Field_group)
 
-# Function to standardize (only if needed) ----
-standr = function(x){(x-min(x))/(max(x)-min(x))} 
+# Soil sample abundance information in field
+Field_otu_raw <- read.xlsx("Field_data_raw_ASVs.xlsx", sheet = "raw_otu", colNames = T, rowNames = T)
+rownames(Field_otu_raw) <- Field_otu_raw$ASVs_ID
+Field_otu_raw <- Field_otu_raw[ ,Field_group$Sample_ID]
+Field_otu_raw[1:6, 1:6]
+dim(Field_otu_raw)
+Field_otu_raw = Field_otu_raw[rowSums(Field_otu_raw) > 0, ]
+#View(as.data.frame(rowSums(Field_otu_raw)))
 
-# Obtain the Simpson pair-wise dissimilarity (Species Level)
-# Add group information
-Group_otu <- read.xlsx("Greenhouse_data_group.xlsx", sheet = "green_group", colNames = T, rowNames = F)
+# loading sample grouping information in greenhouse exp.
+Green_group <- read.xlsx("Greenhouse_data_group.xlsx", sheet = "green_group", colNames = T, rowNames = T)
+Green_group$Sample_ID <- rownames(Green_group)
+colnames(Green_group)
 
-# Loading the soil fungal abundance samples from greenhouse experiments
-Green_otu <- read.xlsx("Greenhouse_data_raw_ASVs.xlsx", sheet = "raw_ASVs", colNames = T, rowNames = T)
-Green_otu <- Green_otu[ ,Group_otu$Sample_ID]
-colSums(Green_otu)
-Green_otu_01 <- t(Green_otu)
-Green_otu_01[Green_otu_01>0] = 1 
-#View(Green_otu_01)
-fd <- beta.pair(Green_otu_01, index.family = "sorensen")
-Green_total_dis5 <- fd$beta.sim
-Green_dist <- as.matrix(Green_total_dis5)
+# loading sample grouping information in greenhouse exp.
+Green_otu_raw <- read.xlsx("Greenhouse_data_raw_ASVs.xlsx", sheet = "raw_ASVs", colNames = T, rowNames = T)
+rownames(Green_otu_raw) <- Green_otu_raw$ASV_ID
+Green_otu_raw <- Green_otu_raw[ ,Green_group$Sample_ID]
+dim(Green_otu_raw)
+Green_otu_raw <- Green_otu_raw[rowSums(Green_otu_raw) > 0, ]
+Green_otu_raw[1:6, 1:6]
+sum(Green_otu_raw)
 
-# Transform sample-level dissimilarity matrix into species-level dissimilarity matrix
-Green_dist_data <- reshape2::melt(Green_dist, varnames = c("Sample_ID_A", "Sample_ID_B"), value.name = "dist", na.rm = T)
-colnames(Green_dist_data)[1] <- "Sample_ID"
-Green_dist_data <- Green_dist_data %>% left_join(Group_otu[,c("Sample_ID","Species")], by = "Sample_ID")
-colnames(Green_dist_data)[c(1,2)] <- c("Sample_ID2","Sample_ID")
-Green_dist_data <- Green_dist_data %>% left_join(Group_otu[,c("Sample_ID","Species")], by = "Sample_ID")
-aa <- Rmisc::summarySE(Green_dist_data, measurevar = c("dist"), groupvars = c("Species.x", "Species.y"))
-Fungal_dist <- reshape2::dcast(aa, Species.x  ~ Species.y , value.var = "dist")
-rownames(Fungal_dist) <- Fungal_dist$Species.x
-Fungal_dist <- Fungal_dist[,-1]
-diag(Fungal_dist) <- 0
+# show venn plot
+field_ASV <- data.frame(ASV = rownames(Field_otu_raw), type = "Field")
+green_ASV <- data.frame(ASV = rownames(Green_otu_raw), type = "Green")
 
-# standardization
-Fungal_dist <- standr(Fungal_dist)
+################################# Figure S3a ###################################
+list(`Field survey`=field_ASV$ASV, `Greenhouse experiment` = green_ASV$ASV) %>% 
+  ggvenn(show_percentage = F,show_elements = F,label_sep = ",",
+         digits = 1, stroke_color = "white",
+         fill_color = c("#183C63", "#93C8C0"),
+         set_name_color = c("#183C63", "#93C8C0")) -> Figure_S3a; Figure_S3a
 
-# Sloop
-Species <- unique(Group_otu$Species)
-M <- 2
-final_sp2 <- NULL
-final_sp4 <- NULL
-final_sp8 <- NULL
-final_sp14 <- NULL
-final_sp22 <- NULL
-final_sp32 <- NULL
-final_sp44 <- NULL
 
-set.seed(123456)
-for (S in Species) {
-  # Target species
-  remain_sp <- unique((subset(Group_otu, Species != S))$Species)
-  for (N in 1:500) {
-    ### 2
-    sample_sp2 <- sample(remain_sp, M, replace = FALSE)
-    sample_sp_dist2 <- Fungal_dist[c(S,sample_sp2), c(S,sample_sp2)]
-    sp_dist2_DI <- data.frame(colSums(sample_sp_dist2)/(nrow(sample_sp_dist2)-1));colnames(sp_dist2_DI) = "Fungal_Di"
-    sp_dist2_DI$taxon <- rownames(sp_dist2_DI)
-    sp_dist2_DI$num <- M
-    sp_dist2_DI <- sp_dist2_DI[S,]
-    final_sp2 <- rbind(final_sp2, sp_dist2_DI)
-    ### 4
-    sample_sp4 <- sample(remain_sp, M+2)
-    sample_sp_dist4 <- Fungal_dist[c(S,sample_sp4), c(S,sample_sp4)]
-    sp_dist4_DI <- data.frame(colSums(sample_sp_dist4)/(nrow(sample_sp_dist4)-1));colnames(sp_dist4_DI) = "Fungal_Di"
-    sp_dist4_DI$taxon <- rownames(sp_dist4_DI)
-    sp_dist4_DI$num <- M+2
-    sp_dist4_DI <- sp_dist4_DI[S,]
-    final_sp4 <- rbind(final_sp4, sp_dist4_DI)
-    ### 8
-    sample_sp8 <- sample(remain_sp, M+6)
-    sample_sp_dist8 <- Fungal_dist[c(S,sample_sp8), c(S,sample_sp8)]
-    sp_dist8_DI <- data.frame(colSums(sample_sp_dist8)/(nrow(sample_sp_dist8)-1));colnames(sp_dist8_DI) = "Fungal_Di"
-    sp_dist8_DI$taxon <- rownames(sp_dist8_DI)
-    sp_dist8_DI$num <- M+6
-    sp_dist8_DI <- sp_dist8_DI[S,]
-    final_sp8 <- rbind(final_sp8, sp_dist8_DI)
-    ### 14
-    sample_sp14 <- sample(remain_sp, M+12)
-    sample_sp_dist14 <- Fungal_dist[c(S,sample_sp14), c(S,sample_sp14)]
-    sp_dist14_DI <- data.frame(colSums(sample_sp_dist14)/(nrow(sample_sp_dist14)-1));colnames(sp_dist14_DI) = "Fungal_Di"
-    sp_dist14_DI$taxon <- rownames(sp_dist14_DI)
-    sp_dist14_DI$num <- M+12
-    sp_dist14_DI <- sp_dist14_DI[S,]
-    final_sp14 <- rbind(final_sp14, sp_dist14_DI)
-    ### 22
-    sample_sp22 <- sample(remain_sp, M+20)
-    sample_sp_dist22 <- Fungal_dist[c(S,sample_sp22), c(S,sample_sp22)]
-    sp_dist22_DI <- data.frame(colSums(sample_sp_dist22)/(nrow(sample_sp_dist22)-1));colnames(sp_dist22_DI) = "Fungal_Di"
-    sp_dist22_DI$taxon <- rownames(sp_dist22_DI)
-    sp_dist22_DI$num <- M+20
-    sp_dist22_DI <- sp_dist22_DI[S,]
-    final_sp22 <- rbind(final_sp22, sp_dist22_DI)
-    ### 32
-    sample_sp32 <- sample(remain_sp, M+30)
-    sample_sp_dist32 <- Fungal_dist[c(S,sample_sp32), c(S,sample_sp32)]
-    sp_dist32_DI <- data.frame(colSums(sample_sp_dist32)/(nrow(sample_sp_dist32)-1));colnames(sp_dist32_DI) = "Fungal_Di"
-    sp_dist32_DI$taxon <- rownames(sp_dist32_DI)
-    sp_dist32_DI$num <- M+30
-    sp_dist32_DI <- sp_dist32_DI[S,]
-    final_sp32 <- rbind(final_sp32, sp_dist32_DI)
-    ### 44
-    sample_sp44 <- sample(remain_sp, M+42)
-    sample_sp_dist44 <- Fungal_dist[c(S,sample_sp44), c(S,sample_sp44)]
-    sp_dist44_DI <- data.frame(colSums(sample_sp_dist44)/(nrow(sample_sp_dist44)-1));colnames(sp_dist44_DI) = "Fungal_Di"
-    sp_dist44_DI$taxon <- rownames(sp_dist44_DI)
-    sp_dist44_DI$num <- M+42
-    sp_dist44_DI <- sp_dist44_DI[S,]
-    final_sp44 <- rbind(final_sp44, sp_dist44_DI)
-    print(paste("Species:", S, "Repeat:", N))
+################################# Figure S3b ###################################
+# Filter common ASV IDs
+common_ASVs <- intersect(rownames(Field_otu_raw), rownames(Green_otu_raw))
+length(common_ASVs)
+
+# converted the raw ASVs abundance data to relative abundances in greenhouse exp.
+Green_otu_rel <- apply(Green_otu_raw, 2, function(x) x / sum(x))
+Green_otu_rel <- as.data.frame(Green_otu_rel)
+colSums(Green_otu_rel)
+
+Green_long_abun <- data.frame(ASV = row.names(Green_otu_rel), Green_otu_rel) %>%
+  tidyr::pivot_longer(cols = -ASV, names_to = "Sample_ID",values_to = "abun") %>%
+  left_join(Green_group[,c("Sample_ID", "Species")])
+head(Green_long_abun)
+
+Green_mean_rel = Green_long_abun %>% group_by(Species, ASV) %>%
+  summarise(mean_abun = mean(abun))
+head(Green_mean_rel)
+
+Green_mean_matrix <- Green_mean_rel %>%
+  tidyr::pivot_wider(names_from = Species, values_from = mean_abun, values_fill = 0) %>%
+  tibble::column_to_rownames("ASV") 
+colSums(Green_mean_matrix)
+
+# converted the raw ASVs abundance data to relative abundances in field survey
+Field_otu_rel <- apply(Field_otu_raw, 2, function(x) x / sum(x))
+Field_otu_rel <- as.data.frame(Field_otu_rel)
+colSums(Field_otu_rel)
+
+# The contribution of shared taxa to the variation in community composition was 
+# calculated separately for each site and year.
+
+Years <- unique(Field_group$Years)
+Site <- unique(Field_group$Site)
+diff_BC_merge_all <- NULL
+
+for (i in Years) {
+  for (ii in Site) {
+    sub_group = subset(Field_group, Years == i & Site == ii)
+    
+    # Field survey
+    Field_otu_rel_sub = Field_otu_rel[,sub_group$Sample_ID]
+    
+    # Step 1: Calculate compositional variation of the entire community.
+    x <- apply(combn(ncol(Field_otu_rel_sub), 2), 2, function(x) sum(abs(Field_otu_rel_sub[,x[1]] - Field_otu_rel_sub[,x[2]]))/2 )
+    x_names <- apply(combn(ncol(Field_otu_rel_sub), 2), 2, function(x) paste(colnames(Field_otu_rel_sub)[x], collapse=' - '))
+    Field_BC_full <- data.frame(x, x_names)
+    
+    # Step 2: Calculate compositional variation of the common community.
+    Field_common_sub <- Field_otu_rel_sub[common_ASVs, ]
+    x <- apply(combn(ncol(Field_common_sub), 2), 2, function(x) sum(abs(Field_common_sub[, x[1]] - Field_common_sub[, x[2]]))/2)
+    x_names <- apply(combn(ncol(Field_common_sub), 2), 2, function(x) paste(colnames(Field_common_sub)[x], collapse=' - '))
+    Field_BC_common <- data.frame(x, x_names)
+    
+    # add site, year information
+    diff_BC_Field <- left_join(Field_BC_full, Field_BC_common, by=c('x_names'))
+    diff_BC_Field$diff_BC <- diff_BC_Field$x.y/diff_BC_Field$x.x
+    diff_BC_Field$Years = i; diff_BC_Field$Site = ii; diff_BC_Field$Type = "Field"
+    
+    # add sample information
+    split_names <- strsplit(diff_BC_Field$x_names, " - ")
+    diff_BC_Field$Sample_ID1 <- sapply(split_names, function(x) x[1])
+    diff_BC_Field$Sample_ID2 <- sapply(split_names, function(x) x[2])
+    
+    # add species information
+    diff_BC_Field$Species_1 <- sub_group$Species[match(diff_BC_Field$Sample_ID1, rownames(sub_group))]
+    diff_BC_Field$Species_2 <- sub_group$Species[match(diff_BC_Field$Sample_ID2, rownames(sub_group))]
+    
+    diff_BC_Field$Species_pair = paste0(diff_BC_Field$Species_1, " - ", diff_BC_Field$Species_2)
+    colnames(diff_BC_Field)[2] = "Sample_pair"
+    
+    
+    # Greenhouse experiment
+    
+    # Step 1: Calculate compositional variation of the entire community.
+    Green_otu_rel_sub = Green_mean_matrix[, sub_group$Species]
+    
+    x <- apply(combn(ncol(Green_otu_rel_sub), 2), 2, function(x) sum(abs(Green_otu_rel_sub[,x[1]] - Green_otu_rel_sub[,x[2]]))/2 )
+    x_names <- apply(combn(ncol(Green_otu_rel_sub), 2), 2, function(x) paste(colnames(Green_otu_rel_sub)[x], collapse=' - '))
+    Green_BC_full <- data.frame(x, x_names)
+    
+    # Step 2: Calculate compositional variation of the common community.
+    Green_common_matrix <- Green_otu_rel_sub[common_ASVs, ]
+    x <- apply(combn(ncol(Green_common_matrix), 2), 2, function(x) sum(abs(Green_common_matrix[, x[1]] - Green_common_matrix[, x[2]]))/2)
+    x_names <- apply(combn(ncol(Green_common_matrix), 2), 2, function(x) paste(colnames(Green_common_matrix)[x], collapse=' - '))
+    Green_BC_common <- data.frame(x, x_names)
+    
+    # add site, year information
+    diff_BC_Green <- left_join(Green_BC_full, Green_BC_common, by=c('x_names'))
+    #head(diff_BC_Green)
+    diff_BC_Green$diff_BC <- diff_BC_Green$x.y/diff_BC_Green$x.x
+    #mean(diff_BC_Green$diff_BC)
+    diff_BC_Green$Years = i; diff_BC_Green$Site = ii; diff_BC_Green$Type = "Greenhouse"
+    
+    # add sample information
+    split_names <- strsplit(diff_BC_Green$x_names, " - ")
+    diff_BC_Green$Species_1 <- sapply(split_names, function(x) x[1])
+    diff_BC_Green$Species_2 <- sapply(split_names, function(x) x[2])
+    
+    # add species information
+    diff_BC_Green$Sample_ID1 <- sub_group$Sample_ID[match(diff_BC_Green$Species_1, sub_group$Species)]
+    diff_BC_Green$Sample_ID2 <- sub_group$Sample_ID[match(diff_BC_Green$Species_2, sub_group$Species)]
+    
+    diff_BC_Green$Sample_pair = paste0(diff_BC_Green$Sample_ID1, " - ", diff_BC_Green$Sample_ID2)
+    colnames(diff_BC_Green)[2] = "Species_pair"
+    
+    # merge dataset
+    # reorder
+    diff_BC_Green = diff_BC_Green[,colnames(diff_BC_Field)]
+    
+    diff_BC_merge = rbind(diff_BC_Field, diff_BC_Green)
+    
+    # sloop 
+    diff_BC_merge_all = rbind(diff_BC_merge_all, diff_BC_merge)
+    
   }
 }
 
-total_di <- rbind(final_sp2,final_sp4,final_sp8,final_sp14,final_sp22,final_sp32,final_sp44)  
-#head(total_di)
-total_di$Species <- gsub("_", " ", total_di$taxon)
-#unique(total_di$taxon)
 
-# rename of Latin names
-first_char <- substr(total_di$Species, 1, 1)
-sub_str <- gsub(".*_", "", total_di$taxon)
-Latin_name <- paste(first_char, sub_str, sep = ". ")
-total_di$Latin_name <- Latin_name
+# set color of site
+site_colors <- c("Guangzhou" = "#87898A", "Guilin" = "#C26275", "Changsha" = "#41479F",
+                 "Wuhan" = "#32B7B2", "Zhengzhou" = "#75A750", "Tai'an" = "#E69F0D")
 
-#plot
-ggplot() +
-  #geom_jitter(total_di, mapping = aes(x = as.factor(num) , y = (Fungal_Di)),
-  #position = position_jitter(0.3), size=0.3, color = "#00000022") +  #fill = "#00000022", stroke = 2
-  labs(x = 'Add number of species',
-       y = 'Fungal compositional distinctiveness')+
-  geom_boxplot(data = total_di, mapping = aes(x = as.factor(num) , y = (Fungal_Di)), outlier.size = 0.8, outlier.shape = 21, size = 0.5) + 
-  facet_wrap(~ Latin_name,ncol = 9, nrow = 6) + 
-  mytheme + 
-  theme(strip.text = element_text(size = 9, face = "italic"),
-        #strip.background = element_rect(colour = "black"),
-        axis.text = element_text(colour = 'black', size = 8)) ->  fig_S3
+diff_BC_merge_all$Site <- factor(diff_BC_merge_all$Site, levels = c("Guangzhou","Guilin","Changsha","Wuhan","Zhengzhou","Tai'an"))
+diff_BC_merge_all$Type <- factor(diff_BC_merge_all$Type, levels = c("Field","Greenhouse"))
+diff_BC_merge_all$Years <- as.factor(diff_BC_merge_all$Years)
 
-# one-way Anova
-taxon <- unique(total_di$taxon)
-final_data <- NULL
 
-for (i in taxon) {
-  select_data <- subset(total_di, taxon == i)
-  mod <- aov(Fungal_Di ~ factor(num), data = select_data)
-  results <- summary(mod)
-  summary_data <- data.frame(Species = i, F_value = results[[1]][1,4], p_value = results[[1]][1,5])
-  final_data <- rbind(final_data, summary_data)
-}
+diff_BC_merge_all %>% 
+  group_by(Type) %>% 
+  summarise(
+    mean_diff_BC = mean(diff_BC*100),
+    sd_diff_BC = sd(diff_BC*100),
+    se_diff_BC = sd(diff_BC*100) / sqrt(n()))
 
-print(subset(final_data, p_value <= 0.05))
+# plot
+ggplot(diff_BC_merge_all, aes(y = Years, x = diff_BC*100, fill = Type, color = Type)) +
+  geom_density_ridges(rel_min_height = 0.01, scale = 0.9, alpha = 0.5, linewidth = 0.7,
+                      quantile_lines = TRUE, quantile_fun = mean) +
+  scale_color_manual(values = c("Field" = "#1C3C63", "Greenhouse" = "#93C8C0")) +
+  scale_fill_manual(values = c("Field" = "#1C3C63", "Greenhouse" = "#93C8C0")) +
+  scale_x_continuous(labels = scales::label_comma(accuracy = 1), limits = c(0,100), 
+                     breaks = seq(0, 100, by = 25), expand = expansion(mult = c(0, 0.1))) +
+  scale_y_discrete(expand = expansion(mult = c(0.36, 0))) + 
+  geom_vline(xintercept = 50, linetype = 2) +
+  ggh4x::facet_grid2( ~ Site, #switch = "y", 
+                      strip = ggh4x::strip_themed(background_x = ggh4x::elem_list_rect(fill = site_colors))) +
+  theme_minimal() +
+  theme(legend.position = c(0.9,0.15), 
+        legend.title = element_blank(),
+        legend.key = element_blank(),
+        legend.text = element_text(size = 10),
+        axis.line.y = element_line(color = 'black'),
+        axis.ticks = element_line(color = 'black'),
+        axis.title.x = element_text(colour = 'black', size = 14),
+        axis.title.y = element_text(colour = 'black', size = 14),
+        axis.text.y = element_text(colour = 'black', size = 12),
+        axis.text.x = element_text(colour = 'black', size = 12, angle = 90, vjust = -0.01),
+        strip.background = element_rect(color=NA, size=0.5, linetype="solid"),
+        strip.placement = "outside",
+        strip.text.x = element_text(size = 12, colour = "black"),
+        panel.spacing = unit(0, "lines")) +
+  labs(x = 'Common taxa contributions\nto fungal community dissimilarity', y = NULL) +
+  geom_segment(aes(x = 0, xend = 0, y = 1, yend = 3), color = "black") +
+  coord_flip() -> Figure_S3b; Figure_S3b
 
-# 
-sp_select <- subset(final_data, p_value <= 0.05)$Species
-
-for (sp_sel in sp_select) {
-  total_di$num <- as.factor(total_di$num)
-  select_data <- subset(total_di, taxon == sp_sel)
-  mod <- aov(Fungal_Di ~ (num), data = select_data)
-  print(sp_sel);print(summary(mod))
-  LSSeasonNrate <- emmeans(mod, ~ num, adjust = "Tukey")
-  Multiple_comp <- cld(LSSeasonNrate, alpha = 0.05, Letters = letters, adjust = "none", sort = F, rev = TRUE) 
-  print(Multiple_comp)
-}
-
+### Notice that,
+### For more picture details, we have further adjusted it in Adobe illustrator.
